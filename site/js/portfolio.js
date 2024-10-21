4c1a5f3ae32754639cd5e0033ad9c768cc3e8ecb
Maxime TRICOCHE <maxime.trche@gmail.com>
2024-10-21 11:13:23 +0200

    finish JS step 2  in part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,3 +3,13 @@ const avatarImg = document.querySelector(".avatar");
 avatarImg.addEventListener("click", () => {
 	avatarImg.src = "./image/avatar.svg";
 });
+
+// Modify text and color button
+const firstName = document.querySelector("#firstname");
+const changeNameButton = document.querySelector("#change-name");
+
+changeNameButton.addEventListener("click", () => {
+	const userName = prompt("What's your name ?");
+	firstName.textContent = userName;
+	firstName.style.color = "white";
+});
