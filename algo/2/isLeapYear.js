dde25523a8ca1c6d3e7d1cea1713196f92f30d34
Benoît Vandanjon <vandanjon@gmail.com>
2024-10-21 14:54:43 +0200

    [enhance] : simplify readability

--- algo/2/isLeapYear.js
+++ algo/2/isLeapYear.js
@@ -2,20 +2,18 @@
 Écris une fonction qui peut déterminer si une année est une année bissextile. Elle doit renvoyer `true` si c'est le cas, et `false` sinon.
 Rappel : Une année bissextile vérifie **une** de ces règles :
 - Elle est divisible par 4, sans être divisible par 100
+
+OU
+
+
 - Elle est divisible par 400
 Ex : 2004, 2016 et 2020 sont des années bissextiles
 Rappel : Pour vérifier si un nombre est divisible par un autre, tu peux utiliser l'opérateur "modulo" (%)
 */
 
 function isLeapYear(year) {
-  if (year % 400 === 0) {
+  if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
     return true;
-  }
-
-  if (year % 4 === 0) {
-    if (year % 100 !== 0) {
-      return true;
-    }
   }
 
   return false;
