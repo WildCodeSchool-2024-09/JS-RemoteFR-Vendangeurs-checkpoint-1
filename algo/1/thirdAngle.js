1dde68641ec26ae520e6d9c2ee2be884ddaa8594
jerem2802 <creamedoc@gmail.com>
2024-10-21 09:37:01 +0200

    Algo1

--- algo/1/thirdAngle.js
+++ algo/1/thirdAngle.js
@@ -7,7 +7,8 @@ thirdAngle(20, 80) doit retourner 80
 */
 
 function thirdAngle(a, b) {
-  // Your code here !
+	return 180(angle1 + angle2);
 }
+console.log(thirdAngle);
 
 module.exports = thirdAngle;
