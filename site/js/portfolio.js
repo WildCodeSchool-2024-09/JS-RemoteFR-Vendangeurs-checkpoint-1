463c786da38fc60a59de218f8f40e6e9c6e0b778
Benoît Vandanjon <vandanjon@gmail.com>
2024-10-21 17:09:42 +0200

    c'est moche mais c'est vendredi j'ai le droit

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -7,3 +7,41 @@ profileImage.addEventListener("click", () => {
     profileImage.src = "image/avatar-bis.png";
   }
 });
+
+// STEP 2 + 3 + 4
+const userFirstName = document.querySelector("#firstname");
+const modifyUserButton = document.querySelector("#modifyUserButton");
+
+const backgrounds = document.querySelectorAll(".pink-bg");
+const texts = document.querySelectorAll(".pink-text");
+
+modifyUserButton.addEventListener("click", () => {
+  const newColor = prompt("Gimme your color!");
+  const newName = prompt("Entrez votre nom !");
+
+  userFirstName.textContent = newName;
+  userFirstName.style.color = "white";
+
+  backgrounds.forEach((bg) => (bg.style.backgroundColor = newColor));
+  texts.forEach((text) => (text.style.color = newColor));
+});
+
+// SUPER BONUS
+
+const backendSkills = document.querySelector(".column ul");
+const button = document.querySelector("#button-add-elements");
+const input = document.querySelector("#back-elements");
+
+button.addEventListener("click", () => {
+  // je stocke la valeur de l'input.
+  const inputValue = input.value;
+
+  // je crée un élément LI
+  const newLI = document.createElement("li");
+
+  // je rajoute la valeur de l'input en tant que texte.
+  newLI.innerText = inputValue;
+
+  // on rajoute le LI en tant qu'enfant de UL
+  backendSkills.appendChild(newLI);
+});
