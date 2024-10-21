6bb6285d560b7869d7b65b95bd69d3ebcbf1920f
Leslaures <lauremaffini@gmail.com>
2024-10-21 11:57:03 +0200

    step 1 & 2 du JS done

---
+++ site/js/portfolio.js
@@ -0,0 +1,17 @@
+// As a user I can change the avatar image when I click on it
+
+const avatarImg = document.getElementById("avatar_img");
+
+avatarImg.addEventListener("click", () => {
+  avatarImg.src = "./image/avatar-bis.png";
+});
+
+// As a user I can replace the text Matt on the paragraphe with your name and it's color with the color white
+
+const button = document.getElementById("changeNameButton");
+const firstname = document.getElementById("firstname");
+
+button.addEventListener("click", () => {
+  let username = prompt("What's your name?");
+  firstname.innerHTML = username;
+});
