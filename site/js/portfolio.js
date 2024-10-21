7b269c670d25a34a5779d8b1c9000e533847603f
Maxime TRICOCHE <maxime.trche@gmail.com>
2024-10-21 11:26:21 +0200

    finish JS step 3  in part 2

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -7,9 +7,16 @@ avatarImg.addEventListener("click", () => {
 // Modify text and color button
 const firstName = document.querySelector("#firstname");
 const changeNameButton = document.querySelector("#change-name");
+const bgColorSection = document.querySelector("section.pink-bg");
+const bgColorFooter = document.querySelector("footer.pink-bg");
 
 changeNameButton.addEventListener("click", () => {
+	const choiceBgColor = prompt("What color do you want ?");
 	const userName = prompt("What's your name ?");
 	firstName.textContent = userName;
 	firstName.style.color = "white";
+	bgColorSection.style.backgroundColor = choiceBgColor;
+	bgColorFooter.style.backgroundColor = choiceBgColor;
 });
+
+console.log(bgColorFooter);
