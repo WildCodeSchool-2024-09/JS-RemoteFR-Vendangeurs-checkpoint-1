fbd1f9085abcc67f8c1c4e4718671bcf0c8dd6e0
Pene891 <pene1086@gmail.com>
2024-10-21 13:36:42 +0200

    LeapYear

--- algo/2/isLeapYear.js
+++ algo/2/isLeapYear.js
@@ -8,7 +8,13 @@ Rappel : Pour vérifier si un nombre est divisible par un autre, tu peux utilise
 */
 
 function isLeapYear(year) {
-  // Your code here !
+  if ((year % 100 !== 0 && year % 4 ===0 && year % 400 ===0))  // Your code here !
+  return true;
+} else {
+  return false;
 }
+
+console.log(isLeapYear(2020));
+
 
 module.exports = isLeapYear;
