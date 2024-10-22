1c1142f933aca7b0084fe4ab0f04edc60f4f8845
MateuszPlebanek <matprocom@gmail.com>
2024-10-22 20:44:00 +0200

    [feat] : solve step 2

---
+++ site/js/portfolio.js
@@ -0,0 +1,6 @@
+const avatarImage = document.getElementById('avatar-image');
+const avatarInput = document.getElementById('avatar-input');
+
+avatarImage.addEventListener('click', function() {
+avatarInput.click();
+});
